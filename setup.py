977b5c26eca379616b415d9be59bc4db29803b30
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2025-06-24 21:10:45 -0800

    Revert changes
    (mostly)

--- setup.py
+++ setup.py
@@ -3,18 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='TkToolTip',
     version='1.10',
+    author='Nenotriple',
+    url='https://github.com/Nenotriple/TkToolTip',
+    description='Add customizable tooltips to any Tkinter widget.',
     packages=find_packages(),
     install_requires=[],
-    author='Nenotriple',
-    author_email='...',
-    description='Add customizable tooltips to any Tkinter widget.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Nenotriple/TkToolTip',
-    python_requires='>=3.10',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
 )
