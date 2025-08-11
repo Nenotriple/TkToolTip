4efd6d0d50f978c65b4d313fe6eba12cda2faecc
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2025-08-10 20:39:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+readme = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='TkToolTip',
@@ -6,6 +9,10 @@ setup(
     author='Nenotriple',
     url='https://github.com/Nenotriple/TkToolTip',
     description='Add customizable tooltips to any Tkinter widget.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
     install_requires=[],
+    python_requires=">=3.10",
 )
